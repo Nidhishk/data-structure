7917776ab6f29861196be613e00f732633285278
Nidhishk <nidhish1802@gmail.com>
2024-07-21 00:26:14 +0530

    Add Merge and Quick Sort Program

--- Sorting/src/QuickSort.java
+++ Sorting/src/QuickSort.java
@@ -1,2 +1,42 @@
 public class QuickSort {
+    public static void main(String[] args) {
+        int arr[] = {7, 8, 3, 1, 2};
+        quickSort(arr, 0, arr.length - 1);
+
+        for(int i=0;i<arr.length;i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public static int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+
+                //swap
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        i++;
+        // swap with pivot
+        int temp = arr[i];
+        arr[i] = arr[high];
+        arr[high] = temp;
+        return i;
+    }
 }
