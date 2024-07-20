9daba3043ff88eafc14ff1c8eb4e34a5ca181791
Nidhishk <nidhish1802@gmail.com>
2024-07-20 16:13:08 +0530

    Adding Project Skeleton

---
+++ Sorting/src/BubbleSort.java
@@ -0,0 +1,21 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int arr[] = {7, 3, 8, 1, 2};
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        printArray(arr);
+    }
+
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
